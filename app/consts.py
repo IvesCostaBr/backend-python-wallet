f8b550521b4e8b5948626695404987e680796b7f
IvesCostaBr <ivespauiniam@gmail.com>
2021-11-17 09:39:16 -0500

    :hammer: Adicioanndo .env na documentaçao

--- app/consts.py
+++ app/consts.py
@@ -8,7 +8,7 @@ SERVER_ROUTE = {
     "url":"127.0.0.1",
     "port":8000
 }
-DEBUG=False
+DEBUG=os.getenv('DEBUG', default=False)
 API_URL_REQUEST = os.getenv('API_URL_REQUEST')
 DATABASE_LOCAL="sqlite:///order.db"
 DATABASE_URL= f"postgresql+psycopg2://{os.getenv('USER_DATABASE')}:{os.getenv('PASSWORD_DATABASE')}@{os.getenv('DATABASE_URL')}:{os.getenv('PORT_DATABASE')}/{os.getenv('DATABASE')}"
