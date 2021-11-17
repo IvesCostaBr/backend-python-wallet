6bf07e769f53ce7859280d7346239706af87db81
IvesCostaBr <ivespauiniam@gmail.com>
2021-11-17 09:27:04 -0500

    :sparkles: Configuraçao da base de dados de produçao

--- app/consts.py
+++ app/consts.py
@@ -8,5 +8,8 @@ SERVER_ROUTE = {
     "url":"127.0.0.1",
     "port":8000
 }
-DEBUG=True
+DEBUG=False
 API_URL_REQUEST = os.getenv('API_URL_REQUEST')
+DATABASE_LOCAL="sqlite:///order.db"
+DATABASE_URL= f"postgresql+psycopg2://{os.getenv('USER_DATABASE')}:{os.getenv('PASSWORD_DATABASE')}@{os.getenv('DATABASE_URL')}:{os.getenv('PORT_DATABASE')}/{os.getenv('DATABASE')}"
+
